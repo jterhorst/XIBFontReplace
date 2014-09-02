0d25d2a40eb8d4ab1359230c7d8844e54c90e4de
Jason Terhorst <yourstruly@jterhorst.com>
2014-09-02 15:29:05 -0500

    Update JTFontReplacer.m

--- XIBFontReplace/JTFontReplacer.m
+++ XIBFontReplace/JTFontReplacer.m
@@ -14,14 +14,21 @@
 {
 	NSString * fileContents = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:nil];
 	fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" type=\"system\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
-	[fileContents writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"HelveticaNeue-Medium\" family=\"Helvetica Neue\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"Helvetica\" family=\"Helvetica\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"HelveticaNeue\" family=\"Helvetica Neue\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"<string key=\"name\">HelveticaNeue-Medium</string>" withString:[NSString stringWithFormat:@"<string key=\"name\">%@</string>", stringFontName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"<string key=\"family\">Helvetica Neue</string>" withString:[NSString stringWithFormat:@"<string key=\"family\">%@</string>", stringFamilyName]];
+    [fileContents writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
 - (void)replaceBoldSystemFontWithFontString:(NSString *)stringFontName familyName:(NSString *)stringFamilyName inFileAtURL:(NSURL *)fileURL
 {
 	NSString * fileContents = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:nil];
 	fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" type=\"boldSystem\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
-	[fileContents writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"HelveticaNeue-Bold\" family=\"Helvetica Neue\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"Helvetica-Bold\" family=\"Helvetica\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    [fileContents writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
 - (void)replaceItalicSystemFontWithFontString:(NSString *)stringFontName familyName:(NSString *)stringFamilyName inFileAtURL:(NSURL *)fileURL
@@ -29,6 +36,15 @@
 	NSString * fileContents = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:nil];
 	fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" type=\"italicSystem\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
 	[fileContents writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
+- (void)replaceLightFontWithFontString:(NSString *)stringFontName familyName:(NSString *)stringFamilyName inFileAtURL:(NSURL *)fileURL
+{
+    NSString * fileContents = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:nil];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"HelveticaNeue-Light\" family=\"Helvetica Neue\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"fontDescription key=\"fontDescription\" name=\"Helvetica-Light\" family=\"Helvetica\"" withString:[NSString stringWithFormat:@"fontDescription key=\"fontDescription\" name=\"%@\" family=\"%@\"", stringFontName, stringFamilyName]];
+    fileContents = [fileContents stringByReplacingOccurrencesOfString:@"<string key=\"name\">HelveticaNeue-Light</string>" withString:[NSString stringWithFormat:@"<string key=\"name\">%@</string>", stringFontName]];
+    [fileContents writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
 @end
